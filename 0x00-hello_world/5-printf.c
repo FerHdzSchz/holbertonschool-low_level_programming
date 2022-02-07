e84ee90e804a4c110f1201666a21b414569328d1
ferhdzschz <fernandohdz915@hotmail.com>
2022-02-06 22:31:55 -0600

    Add file 5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+/**
+ * main - print with proper grammar, but the outcome is a piece of art,
+ *        using printf
+ */
+
+int main(void)
+{
+
+printf("with proper grammar, but the outcine is a piece of art,\n")
+return (0);
+}
