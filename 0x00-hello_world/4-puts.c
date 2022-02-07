cddc97d1a2792bf9e620b28f0749b4bfc39b68ae
ferhdzschz <fernandohdz915@hotmail.com>
2022-02-06 22:23:01 -0600

    Use compliant Betty style in file 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
-/* File that prints a string */
 #include <stdio.h>
-/*Using puts to print string to stdout*/
+/**
+ * main - Using puts to print string to stdout
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
+
 return (0);
 }
