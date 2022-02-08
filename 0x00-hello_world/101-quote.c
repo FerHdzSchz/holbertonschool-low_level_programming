7afa71d85984d1171cc3deebf04a5aae54a902e2
ferhdzschz <fernandohdz915@hotmail.com>
2022-02-07 19:02:52 -0600

    Importing correct library unistd as header to find function write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
-# include <stdio.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Function to print "and that piece of art is useful" -
  * Dora Korpar, 2015-10-19"
@@ -7,6 +8,6 @@
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",60);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",60);
 return (1);
 }
