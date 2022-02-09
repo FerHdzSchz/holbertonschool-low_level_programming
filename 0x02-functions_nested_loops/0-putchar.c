20fb9e2a3c79e77124063172ad413d20c75cea72
ferhdzschz <fernandohdz915@hotmail.com>
2022-02-08 19:03:19 -0600

    Adding readme file and file to print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Function to print "_putchar"
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+puts("_putchar");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+# Functions and nested loops
+
+This directory is used to create first functions in C.
