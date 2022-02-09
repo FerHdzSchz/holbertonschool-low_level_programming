c53ff8624050b12bf3d5d22d497f076856527dae
ferhdzschz <fernandohdz915@hotmail.com>
2022-02-08 19:21:03 -0600

    Using _putchar in the header

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+#include <_putchar.h>
 #include <main.h>
 /**
  * main - Function to print "_putchar"
@@ -6,6 +7,14 @@
  */
 int main(void)
 {
-puts("_putchar");
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar("\n");
 return (0);
 }
