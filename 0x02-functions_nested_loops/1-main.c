4b24b6767a53a6c223e0d80ee3acfe4dfbd2ffa2
ferhdzschz <fernandohdz915@hotmail.com>
2022-02-08 23:35:30 -0600

    Adding 1-main to existing files

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
