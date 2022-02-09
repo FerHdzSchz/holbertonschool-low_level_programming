458b1421ab2eb089dc5e9f132b4c30aaef7f33ad
ferhdzschz <fernandohdz915@hotmail.com>
2022-02-08 23:33:19 -0600

    adding alphabet file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,9 @@
 /**
  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
-void print_alphabet(void)
+
+int main(void)
 {
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-
-	_putchar('\n');
+print_alphabet();
+return (0);
 }
