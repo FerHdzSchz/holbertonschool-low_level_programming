da3924006fb23b871cf3db00cf5f9befa8dcf819
ferhdzschz <fernandohdz915@hotmail.com>
2022-02-08 19:37:46 -0600

    Using main.h in the header

--- 0x02-functions_nested_loops/0._putchar.c
+++ 0x02-functions_nested_loops/0._putchar.c
@@ -6,14 +6,6 @@
  */
 int main(void)
 {
-_putchar("_");
-_putchar("p");
-_putchar("u");
-_putchar("t");
-_putchar("c");
-_putchar("h");
-_putchar("a");
-_putchar("r");
-_putchar("\n");
+print_putchar("_");
 return (0);
 }
