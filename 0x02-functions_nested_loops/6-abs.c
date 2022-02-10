885f8725320aeaf1c6900334cbfd3f830b9da7f8
ferhdzschz <fernandohdz915@hotmail.com>
2022-02-09 20:12:09 -0600

    Adding main.h and function to print absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Function that calculate absolute value of n.
+ * @n: Number to test
+ *
+ * Return: Absolute value of n.
+ */
+
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+return (n);
+}
