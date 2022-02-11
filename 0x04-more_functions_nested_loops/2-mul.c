091e659184d4389f0e14c09e0a9ad81ecc398bc6
ferhdzschz <fernandohdz915@hotmail.com>
2022-02-10 21:41:19 -0600

    aadding files for multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - function that create a multiplication between two integers
+ * @a: first multiplier
+ * @b: second multiplier
+ *
+ * Result: multiplication result
+ */
+
+int mul(int a, int b)
+{
+return (a*b);
+}
