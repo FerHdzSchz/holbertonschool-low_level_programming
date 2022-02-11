c56b3266d8b22290d7b4a0c8fb22f97f4ba64660
ferhdzschz <fernandohdz915@hotmail.com>
2022-02-10 21:12:20 -0600

    Adding file is upper and main

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _issupper -  Function to detect if a character is 
+ *
+ * @c: Character to evaluate
+ *
+ * Return: 1 if it is uppercase 0 otherwise
+ */
+int _isupper(int c)
+{
+if (c>='A' && c<='Z')
+{
+return (1);
+}
+returm (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+# More basic functions and loops
