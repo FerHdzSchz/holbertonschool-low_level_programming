16384d0cb54fbcda3d35c768e59480f16f1bcb5d
ferhdzschz <fernandohdz915@hotmail.com>
2022-02-10 22:00:46 -0600

    aadding files for printing numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- *
  * print_numbers - Print numbers from 0 to 9
  *
  */
@@ -8,9 +7,9 @@
 void print_numbers(void)
 {
 int i;
-for (i = 0; i<= 9; i++)
+for (i = 0; i <= 9; i++)
 {
-_putchar(i+'0');
+_putchar(i + '0');
 }
 _putchar('\n');
 }
