5c42eaede4f3b6e50c6b8071cd61dae1b6402d55
ferhdzschz <fernandohdz915@hotmail.com>
2022-02-14 21:53:53 -0600

    Adding file to print str to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *
+ * _puts - Print a string ending in a new line
+ * @str: String to evaluate
+ *
+ */
+
+void _puts(char *str)
+{
+int len_str = 0;
+while (*(str+len_str) != '\0')
+{
+_putchar(str[len_str]);
+len_str+=1;
+}
+_putchar('\n');
+}
