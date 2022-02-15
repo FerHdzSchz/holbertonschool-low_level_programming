c803b842b84f3dac4cc9a1413da8dc72beab7f64
ferhdzschz <fernandohdz915@hotmail.com>
2022-02-14 20:54:28 -0600

    Adding files to change pointer to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+/**
+ * 0-reset_to_98.c
+ * Fer Hdz
+ * reset_to_98 - Take a pointer to an int as parameter and update it to 98
+ *
+ * @n: pointer to change
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
