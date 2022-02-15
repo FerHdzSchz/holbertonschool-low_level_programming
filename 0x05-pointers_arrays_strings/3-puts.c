08b4cbdbfff15cb8c8362e2ad6f565938843d48a
ferhdzschz <fernandohdz915@hotmail.com>
2022-02-14 21:55:11 -0600

    Adding file to print str to stdout

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- *
  * _puts - Print a string ending in a new line
  * @str: String to evaluate
  *
@@ -9,10 +8,10 @@
 void _puts(char *str)
 {
 int len_str = 0;
-while (*(str+len_str) != '\0')
+while (*(str + len_str) != '\0')
 {
 _putchar(str[len_str]);
-len_str+=1;
+len_str += 1;
 }
 _putchar('\n');
 }
