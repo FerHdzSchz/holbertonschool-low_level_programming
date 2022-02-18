89103ec6cf2e5843f8c846b8113b88f10ba95b3c
ferhdzschz <fernandohdz915@hotmail.com>
2022-02-17 21:26:18 -0600

    Correcting style strncpy files

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,12 +9,13 @@ word_letter = 0;
 while (src[word_letter] != '\0' && word_letter < n)
 {
 dest[word_letter] = src[word_letter];
-word_letter ++;
+word_letter++;
 }
 while (word_letter < n)
 {
 dest[word_letter] = '\0';
 word_letter++;
 }
+
 return (dest);
 }
