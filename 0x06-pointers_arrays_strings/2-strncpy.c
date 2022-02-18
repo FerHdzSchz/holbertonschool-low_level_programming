8aaed005b53d0e262b8c8fba8f6a9136c5478f5b
ferhdzschz <fernandohdz915@hotmail.com>
2022-02-17 21:25:20 -0600

    Correcting  strncpy files

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,7 +11,10 @@ while (src[word_letter] != '\0' && word_letter < n)
 dest[word_letter] = src[word_letter];
 word_letter ++;
 }
+while (word_letter < n)
+{
 dest[word_letter] = '\0';
-
+word_letter++;
+}
 return (dest);
 }
