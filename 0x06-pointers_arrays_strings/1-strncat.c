ca788ef77a54c6384c3d00df54ee9e5a4998b5ec
ferhdzschz <fernandohdz915@hotmail.com>
2022-02-17 21:11:13 -0600

    Adding strncat files

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,13 @@
 #include "main.h"
+/**
+ * _strncat - Concat 2 words with truncated 1st word
+ *
+ * @dest: 2nd word
+ * @src: 1st word
+ * @n: number of chars from 1st word
+ *
+ * Return: cocatenated string
+ */
 
 char *_strncat(char *dest, char *src, int n)
 {
