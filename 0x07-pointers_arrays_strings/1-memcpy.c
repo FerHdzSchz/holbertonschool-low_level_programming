378ea497d1b862f9cb05828d1e0ab5e81047f74c
ferhdzschz <fernandohdz915@hotmail.com>
2022-02-21 21:53:25 -0600

    Compliance of Betty style of memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,8 +1,13 @@
 #include "main.h"
 
 /**
+ * _memcpy - Function to copy n memory block from src to dest
  *
+ * @dest: destination
+ * @src: source
+ * @n: n items to copy
  *
+ * Return: destination with copied memory
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
