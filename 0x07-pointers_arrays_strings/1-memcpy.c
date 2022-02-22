11753bfdf201169b50761a094c853ff34ef17461
ferhdzschz <fernandohdz915@hotmail.com>
2022-02-21 21:55:46 -0600

    Compliance of Betty style of memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,12 +12,15 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i;
+int i = 0;
+int j = 0;
 
-for (i = 0; i <= n; i++)
+while (n > 0)
 {
-dest[i] = src[i];
+dest[i] = src[j];
+i++;
+j++;
+n--;
 }
-
 return (dest);
 }
