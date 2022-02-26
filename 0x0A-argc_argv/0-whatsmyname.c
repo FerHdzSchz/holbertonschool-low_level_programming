94663c5e90afa65fb581fdf2c4e7fd642f37e88d
ferhdzschz <fernandohdz915@hotmail.com>
2022-02-25 21:06:37 -0600

    Adding firts files for using arguments in funcitons

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Function to print name of function
+ *
+ */
+
+int main(int argc, char **argv)
+{
+argc = argc;
+printf("%s", *argv);
+return (0);
+}
