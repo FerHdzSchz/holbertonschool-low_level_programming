493c44b1fa75c3aa645012e9345496242b056e47
ferhdzschz <fernandohdz915@hotmail.com>
2022-02-25 21:19:01 -0600

    Adding file that prints number of parameters

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print number of arguments in the function
+ *
+ * @argc: Number of arguments
+ * @argv: Argument list
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+argv = argv;
+printf("%d", argc -1);
+return (0);
+}
