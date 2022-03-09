8b207ee3d85fb62d48e4dcd6e1b1f2c082e4aba4
ferhdzschz <fernandohdz915@hotmail.com>
2022-03-08 21:11:33 -0600

    Adding files for dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE dog.h
+
+/**
+ * struct dog - structure for dog
+ * @name: mame of dog
+ * @age: age of dog
+ * @owner: name of the owner
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
