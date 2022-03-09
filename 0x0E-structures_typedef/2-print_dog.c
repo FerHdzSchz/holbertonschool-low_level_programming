7d14171559cc47252f9180bc2973e6179646975c
ferhdzschz <fernandohdz915@hotmail.com>
2022-03-08 21:39:43 -0600

    Adding files to print dog structure

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,7 +16,10 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
-		printf("Age: %f\n", d->age);
+		if (d->age != NULL)
+			printf("Age: %f\n", d->age);
+		else
+			printf("Age: (nil)\n");
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
