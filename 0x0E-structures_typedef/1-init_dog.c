e3f4a7807359ef00412b4a3e83ee9cad4e9f096e
ferhdzschz <fernandohdz915@hotmail.com>
2022-03-08 21:21:43 -0600

    Adding initializer for struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - Initializes a structure from dog
+ *
+ * @d: structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
