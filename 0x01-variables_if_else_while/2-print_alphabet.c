441052eccbac2642c4d91886b0c5a4a29fc17788
ferhdzschz <fernandohdz915@hotmail.com>
2022-03-26 11:10:51 -0600

    Files for 2-alphabet excercises

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,17 +3,18 @@
 /**
  * main - function to print alphabet
  *
- * return: Always 0
+ * Return: Always 0
  */
 
 int main(void)
 {
 
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
-		{
-			putchar(c);
-		}
+	{
+		putchar(c);
+	}
 	putchar('\n');
 	return (0);
 }
