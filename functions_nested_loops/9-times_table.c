7245816a2aca42bb393d54d318471ae694576441
FerHdzSchz <fernandohdz915@hotmail.com>
2023-02-23 08:36:18 -0600

    correcting style 9 times tables

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -13,38 +13,36 @@ void times_table(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
+		{
+			if ('0' + (i * j) <= '9')
 			{
-				if ('0' + (i * j) <= '9')
+				c = '0' + (i * j);
+				_putchar(c);
+				if (j < 9)
 				{
-					c = '0' + (i * j);
-					_putchar(c);
-					if(j < 9)
+					_putchar(',')
+					if ((i * (j + 1)) < 10)
 					{
-						if ((i * (j+1)) < 10)
-						{
-							_putchar(',');
-							_putchar(' ');
-							_putchar(' ');
-						}
-						else
-						{
-							_putchar(',');
-							_putchar(' ');
-						}
+						_putchar(' ');
+						_putchar(' ');
 					}
-
-				}
-				else
-				{
-					_putchar('0' + (i*j)/10);
-					_putchar('0' + (i*j)%10);
-					if (j < 9)
+					else
 					{
-						_putchar(',');
 						_putchar(' ');
 					}
 				}
 			}
+			else
+			{
+				_putchar('0' + (i * j) / 10);
+				_putchar('0' + (i * j) % 10);
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
 		_putchar('\n');
 	}
 }
