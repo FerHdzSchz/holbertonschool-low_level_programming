4b3b423ec6feac738561e45b494956be6bd574c3
FerHdzSchz <fernandohdz915@hotmail.com>
2023-02-23 08:30:52 -0600

    correcting 9 times tables

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,36 +6,45 @@
 
 void times_table(void)
 {
-    int i;
-    int j;
-    char c;
+	int i;
+	int j;
+	char c;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j < 10; j++)
-            {
-                if ('0' + (i * j) <= '9')
-                {
-                    c = '0' + (i * j);
-                    _putchar(c);
-                    if (j < 9)
-                    {
-                        _putchar(',');
-                        _putchar(' ');
-                        _putchar(' ');
-                    }
-                }
-                else
-                {
-                    _putchar('0' + (i*j)/10);
-                    _putchar('0' + (i*j)%10);
-                    if (j < 9)
-                    {
-                        _putchar(',');
-                        _putchar(' ');
-                    }
-                }
-            }
-        _putchar('\n');
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+			{
+				if ('0' + (i * j) <= '9')
+				{
+					c = '0' + (i * j);
+					_putchar(c);
+					if(j < 9)
+					{
+						if ((i * (j+1)) < 10)
+						{
+							_putchar(',');
+							_putchar(' ');
+							_putchar(' ');
+						}
+						else
+						{
+							_putchar(',');
+							_putchar(' ');
+						}
+					}
+
+				}
+				else
+				{
+					_putchar('0' + (i*j)/10);
+					_putchar('0' + (i*j)%10);
+					if (j < 9)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+				}
+			}
+		_putchar('\n');
+	}
 }
