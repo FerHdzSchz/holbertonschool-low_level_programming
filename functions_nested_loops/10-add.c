1ad6f5ff5b73dd8fbfbf0bc907918101b8292e84
FerHdzSchz <fernandohdz915@hotmail.com>
2023-02-23 11:53:22 -0600

    correcting betty style add

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,11 +1,18 @@
 #include "main.h"
 /**
  * add - Prints the sum of two numbers.
+ *
+ * @a: integer
+ * @b: integer
+ *
+ * Return: sum of integers
  */
 
 int add(int a, int b)
 {
-    int s;
-    s = a + b;
-    return(s);
+	int s;
+
+	s = a + b;
+
+	return (s);
 }
