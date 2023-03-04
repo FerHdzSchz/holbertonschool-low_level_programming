f488afd2ff2f023abb0155536e4533e957bdc4bd
FerHdzSchz <fernandohdz915@hotmail.com>
2023-03-03 20:00:15 -0600

    more functions

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - function that create a multiplication between two integers
+ * @a: first multiplier
+ * @b: second multiplier
+ *
+ * Return: multiplication result
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
