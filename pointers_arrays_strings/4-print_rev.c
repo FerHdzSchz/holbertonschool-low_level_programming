5bb243d3340bad41a31442f3222cb92643c4051a
FerHdzSchz <fernandohdz915@hotmail.com>
2023-03-08 17:27:45 -0600

    4-print reverse string

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,19 +6,18 @@
 
 void print_rev(char *s)
 {
-    int j, i;
-    int len_str = 0;
-    while (*(s + len_str) != '\0')
-    {
-    len_str += 1;
-    }
+	int j;
+	int len_str;
 
-    char rev_str[len_str];
-    for (j = len_str; j = 0; j--)
-    {
-        i = len_str - j;
-        rev_str[j] = *(s + i);
-    }
+	len_str = 0;
+	while (*(s + len_str) != '\0')
+	{
+		len_str += 1;
+	}
 
-
+	for (j = len_str - 1; j == 0; j--)
+	{
+		_putchar(s[j]);
+	}
+		_putchar('\n');
 }
