c73ff0baa10169ac725e07ae092ec57a67ffa10c
FerHdzSchz <fernandohdz915@hotmail.com>
2023-03-08 21:02:06 -0600

    print n elements of array remove last comma

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -15,7 +15,15 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", a[i]);
-		printf("%c ", ',');
+		if(i == n -1)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+			printf("%c ", ',');
+
+		}
 	}
 }
