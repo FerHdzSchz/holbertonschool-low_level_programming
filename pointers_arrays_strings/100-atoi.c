e7b9ba84892ba9713df82bec25057ebc3aee865a
FerHdzSchz <fernandohdz915@hotmail.com>
2023-03-09 14:14:14 -0600

    atoi func

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+* _atoi - Converts string to number
+*
+* @s: string
+* Return: Converted integer or 0 if no number
+*/
+
+int _atoi(char *s)
+{
+	int n;
+	int sign;
+	int d;
+	int fnum;
+
+	n = 0;
+	sign = 1;
+	fnum = 0;
+
+	while (*(s + n) != '\0')
+	{
+		if (s[n] == '-')
+		{
+			sign *= -1;
+			n++;
+		}
+		else if (s[n] == '+')
+		{
+			sign *= 1;
+			n++;
+		}
+		else if (s[n] >= '0' && s[n] <= '9')
+		{
+			d = s[n] - '0';
+			fnum = fnum * 10 + d;
+			n++;
+		}
+		else
+		{
+			n++;
+		}
+	}
+	return (sign * fnum);
+}
