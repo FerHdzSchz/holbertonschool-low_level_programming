a17acd3e31fd5ffdf41278cc2e82c33dc4644f7d
FerHdzSchz <fernandohdz915@hotmail.com>
2023-03-08 18:15:36 -0600

    adding reverse string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* rev_string - Reverse a string
+*
+* @s: string to reverse
+*
+*/
+
+void rev_string(char *s)
+{
+	int j;
+	int len_str;
+
+	len_str = 0;
+	while (*(s + len_str) != '\0')
+	{
+		len_str += 1;
+	}
+	for (int j = 0; j < len / 2; j++)
+	{
+		char tmp = s[j];
+		s[j] = s[len_str - j - 1];
+		s[len_str - j - 1] = tmp;
+	}
+
+}
