5fd450c8457dc4f12fec3ac73644919864169285
FerHdzSchz <fernandohdz915@hotmail.com>
2023-03-09 12:18:22 -0600

    addind strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strcpy - Copies a string including buffer to dest
+ *
+ * @dest: Destination buffer
+ * @src: Word to copy
+ *
+ * Return: Returns copied string
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int str_len;
+	int i;
+
+	str_len = 0;
+
+	while (*(src + str_len) != '\0')
+	{
+		str_len++;
+	}
+
+	for (i = 0; i <= str_len; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i + 1] = src[i + 1];
+	return (dest);
+}
