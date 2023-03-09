8da2c2ec32c05daed5d89e58bddab7318ba33f43
FerHdzSchz <fernandohdz915@hotmail.com>
2023-03-09 17:03:25 -0600

    adding strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -3,20 +3,29 @@
  * _strchr - Find address of first occurance of character in string
  *
  * @s:string
+ *
  * @c:character to look for
  * Return: Address of first occuranceof character
  */
+
 char *_strchr(char *s, char c)
 {
-int i;
-i = 0;
-while (s[i] != '\0')
-{
-if (s[i] == c)
-{
-return (&s[i]);
-}
-i++;
-}
-return (0);
+    int i;
+
+    i = 0;
+
+    while (s[i] != '\0')
+    {
+        if (s[i] == c)
+    {
+        return (&s[i]);
+    }
+        i++;
+    }
+
+    if(c == '\0')
+    {
+        return s;
+    }
+    return (0);
 }
