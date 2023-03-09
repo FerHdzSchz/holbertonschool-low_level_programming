da13f0629007e7247d81420fb35f584976623e13
FerHdzSchz <fernandohdz915@hotmail.com>
2023-03-08 18:25:32 -0600

    puts 2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* puts2 - prints every other character and finishes with a new line
+*
+* @str: string to reverse
+*
+*/
+
+void void puts2(char *str)
+{
+	int j;
+	int len_str;
+
+	len_str = 0;
+	while (*(s + len_str) != '\0')
+	{
+		len_str += 1;
+	}
+
+	for (j = 0; j < len_str; j++)
+	{
+		if (j % 2 == 0)
+		{
+			_putchar(s[i]);
+		}
+		else
+		{
+			continue;
+		}
+	}
+	_putchar('\n');
+}
