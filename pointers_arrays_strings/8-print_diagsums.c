0947bc8ed39c7620037592d44ae5d5d908404d5a
FerHdzSchz <fernandohdz915@hotmail.com>
2023-03-10 21:36:28 -0600

    diag sum

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - Prints sums of diagonals
+ * @a:matrix
+ * @size:size of matrix col
+ *
+*/
+
+void print_diagsums(int *a, int size)
+{
+    int ptr[size][size];
+    int i, j, sumd1, sumd2;
+    sumd1 = 0;
+    sumd2 = 0;
+
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size; j++)
+        {
+
+            ptr[i][j] = *(a + i*size + j);
+
+            if (i == j)
+            {
+                sumd1 += ptr[i][j];
+            }
+            if (i + (size - j) == size)
+            {
+                sumd2 += ptr[i][j];
+            }
+        }
+    }
+    printf("%d; %d\n", sumd1, sumd2);
+
+}
