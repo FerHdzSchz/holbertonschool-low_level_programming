be30844cd1f2da4a3f8035911c5fc6eab1257156
FerHdzSchz <fernandohdz915@hotmail.com>
2023-03-10 21:37:29 -0600

    diag sum

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -10,11 +10,13 @@
 
 void print_diagsums(int *a, int size)
 {
-    int ptr[size][size];
-    int i, j, sumd1, sumd2;
+    int i, j, sumd1, sumd2, z;
+    z = size;
+    int ptr[z][z];
+
     sumd1 = 0;
     sumd2 = 0;
-
+    
     for (i = 0; i < size; i++)
     {
         for (j = 0; j < size; j++)
