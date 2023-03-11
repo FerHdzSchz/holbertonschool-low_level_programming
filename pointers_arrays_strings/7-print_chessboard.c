1cdd6c7d762d5a225469b7d81d0134b0a2d17f74
FerHdzSchz <fernandohdz915@hotmail.com>
2023-03-10 21:00:05 -0600

    print chess with betty

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,17 +1,22 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * print_chessboard - Print matrix 8x8
+ *
+ * @a: matrix to print
+ *
+*/
 void print_chessboard(char (*a)[8])
 {
-    unsigned int i, j;
+	unsigned int i, j;
 
 
-    for (i = 0; i < 8; i++)
-    {
-        for (j = 0; j < 8; j++)
-        {
-            _putchar(a[i][j]);
-        }
-        _putchar(10);
-    }
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar(10);
+	}
 }
