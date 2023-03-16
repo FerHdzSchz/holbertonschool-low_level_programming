28f53273a3d7b14111ec3155603c9555b77c58df
FerHdzSchz <fernandohdz915@hotmail.com>
2023-03-16 13:32:33 -0600

    correction 2d allocation

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -28,14 +28,20 @@ int **alloc_grid(int width, int height)
 	array2d = malloc(sizeof(int) * height);
 	if (array2d == NULL)
 	{
+		free(array2d);
 		return (0);
 	}
 	for (h = 0;  h < height; h++)
 	{
 		array2d[h] = malloc(width * sizeof(int));
+		if(array2d[h] == NULL)
+		{
+			return (0);
+		}
 		for (w = 0; w < width; w++)
 		{
 			array2d[h][w] = 0;
+			/*printf("arr[%d][%d] = %d\n", h,w, array2d[h][w]);*/
 		}
 		if (array2d[h] == NULL)
 			return (0);
