368eb99875e586d38d1c112982548da98c84c2c4
FerHdzSchz <fernandohdz915@hotmail.com>
2023-03-15 19:36:47 -0600

    strdup

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -4,42 +4,42 @@
 /**
 * *_strdup - returns pinter to duplicated string
 * @str: input string to be duplicated
-* return: pointer to duplicated string
+* Return: pointer to duplicated string
 */
 
 char *_strdup(char *str)
 {
-    int str_len;
-    int i;
-    char *array;
+	int str_len;
+	int i;
+	char *array;
 
-    str_len = 0;
+	str_len = 0;
 
-    while(*(str + str_len) != '\0')
-    {
-        str_len++;
-    }
-    if (str == '\0')
-    {
-        return (0);
-    }
-    if (str_len == 0)
-    {
-        return (0);
-    }
-    else
-    {
-        array = malloc(sizeof(char) * str_len);
-        if (array == NULL)
-        {
-            return (0);
-        }
-        for (i= 0; i < str_len; i++)
-        {
-            array[i] = str[i];
-        }
-    }
+	while (*(str + str_len) != '\0')
+	{
+		str_len++;
+	}
+	if (str == '\0')
+	{
+		return (0);
+	}
+	if (str_len == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		array = malloc(sizeof(char) * str_len);
+		if (array == NULL)
+		{
+			return (0);
+		}
+		for (i = 0; i < str_len; i++)
+		{
+			array[i] = str[i];
+		}
+	}
 
-    return (array);
+	return (array);
 
 }
