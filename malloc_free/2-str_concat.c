7b2d1f96631254183438fcd551250b95a093f53f
FerHdzSchz <fernandohdz915@hotmail.com>
2023-03-15 21:38:15 -0600

    str concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -19,15 +19,10 @@ char *str_concat(char *s1, char *s2)
 	str1_len = 0;
 	str2_len = 0;
 
-	if ((s1 == NULL) && (s2 == NULL))
-		return (0);
-
-	if (s1 == NULL)
-		while (*(s2 + str2_len) != '\0')
-			str2_len++;
-	if (s2 == NULL)
-		while (*(s1 + str1_len) != '\0')
-			str1_len++;
+	while (*(s2 + str2_len) != '\0')
+		str2_len++;
+	while (*(s1 + str1_len) != '\0')
+		str1_len++;
 	array = malloc((sizeof(char) * (str1_len + str2_len)) + 1);
 	if (array == NULL)
 	{
