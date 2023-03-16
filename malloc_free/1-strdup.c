fdf3f736b75556cba9352d17d63ce7351588bdb5
FerHdzSchz <fernandohdz915@hotmail.com>
2023-03-15 19:33:23 -0600

    strdup

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* *_strdup - returns pinter to duplicated string
+* @str: input string to be duplicated
+* return: pointer to duplicated string
+*/
+
+char *_strdup(char *str)
+{
+    int str_len;
+    int i;
+    char *array;
+
+    str_len = 0;
+
+    while(*(str + str_len) != '\0')
+    {
+        str_len++;
+    }
+
+    if (str_len == 0)
+    {
+        return (0);
+    }
+    else
+    {
+        array = malloc(sizeof(char) * str_len);
+        if (array == NULL)
+        {
+            return (0);
+        }
+        for (i= 0; i < str_len; i++)
+        {
+            array[i] = str[i];
+        }
+    }
+
+    return (array);
+
+}
