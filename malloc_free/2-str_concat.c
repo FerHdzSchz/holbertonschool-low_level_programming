3a48e45d038e0cb9e5f561e800c25ad4e8fa331f
FerHdzSchz <fernandohdz915@hotmail.com>
2023-03-15 20:48:51 -0600

    str concat

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *str_concat - concat s1 and s2
+ *
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: pointer to concat string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int str1_len, str2_len;
+	char *array;
+
+	while (*(s1 + str1_len) != '\0')
+		str1_len++;
+	while (*(s2 + str2_len) != '\0')
+		str2_len++;
+
+	array = malloc((sizeof(char) * (str1_len + str2_len)) + 1);
+
+	for (i = 0; i < (str1_len + str2_len); i++)
+	{
+		if i < str1_len;
+			array[i] = s1[i];
+		else
+			array[i] = s2[i + str1_len];
+	}
+	array[i + 1] = '\0';
+
+	return (array);
+}
