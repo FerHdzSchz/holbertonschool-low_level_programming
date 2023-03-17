75df98e935470b0b685cfddbfc63901124c8205b
FerHdzSchz <fernandohdz915@hotmail.com>
2023-03-17 13:56:28 -0600

    free grid betty style

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -4,19 +4,19 @@
 
 /**
  * free_grid - free grid memory
- * @grid: - grid to clean
- * @heigth: num of rows from grid
+ * @grid:  grid to clean
+ * @height: num of rows from grid
  *
 */
 
 void free_grid(int **grid, int height)
 {
-    int h;
+	int h;
 
-    h = 0;
-    for (h = 0; h < height; h++)
-    {
-        free(grid[h]);
-    }
-    free(grid);
+	h = 0;
+	for (h = 0; h < height; h++)
+	{
+		free(grid[h]);
+	}
+	free(grid);
 }
