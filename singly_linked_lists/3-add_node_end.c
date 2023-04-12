e9ce84e304c60afdc5eb165ce44a1064c8f833ff
FerHdzSchz <fernandohdz915@hotmail.com>
2023-04-11 22:12:33 -0500

    append on single linked list

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -12,13 +12,13 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-    char *aux_str;
-    int str_len;
-    list_t *new_node, *old_nodes;
+	char *aux_str;
+	int str_len;
+	list_t *new_node, *old_nodes;
 
-    new_node = malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
 
-  	if (new_node == NULL)
+	if (new_node == NULL)
 	{
 		return (NULL);
 	}
@@ -31,22 +31,22 @@ list_t *add_node_end(list_t **head, const char *str)
 		str_len++;
 	}
 
-    new_node->str = aux_str;
-    new_node->len = str_len;
-    new_node->next = NULL;
+	new_node->str = aux_str;
+	new_node->len = str_len;
+	new_node->next = NULL;
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-    }
-    else
-    {
-        old_nodes = *head;
-        while (old_nodes->next != NULL)
-        {
-            old_nodes = old_nodes->next;
-        }
-        old_nodes->next = new_node;
-    }
-    return(*head);
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		old_nodes = *head;
+		while (old_nodes->next != NULL)
+		{
+			old_nodes = old_nodes->next;
+		}
+		old_nodes->next = new_node;
+	}
+	return (*head);
 }
