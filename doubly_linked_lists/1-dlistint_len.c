7b1c2b69d9cfbf927862d57c5afb4baee10edfe3
FerHdzSchz <fernandohdz915@hotmail.com>
2023-04-12 21:36:44 -0500

    count elements dlinked list

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+/**
+ * dlistint_len - Count n of elements of double linked list
+ * @h: pointer to head of dlinked list
+ * Return: number of elements in list
+*/
+
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int n;
+	const dlistint_t head = h;
+
+	n = 0;
+	while (head != NULL)
+	{
+		head = head->next;
+		n++;
+	}
+	return (n);
+}
