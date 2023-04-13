a32046e1e5f95ee17f18a3157e8f56015949a63f
FerHdzSchz <fernandohdz915@hotmail.com>
2023-04-12 21:45:46 -0500

    count elements dlinked list

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <stddef.h>
 #include <stdio.h>
 /**
  * dlistint_len - Count n of elements of double linked list
@@ -8,11 +7,10 @@
  * Return: number of elements in list
 */
 
-
 size_t dlistint_len(const dlistint_t *h)
 {
 	int n;
-	const dlistint_t head = h;
+	const dlistint_t *head = h;
 
 	n = 0;
 	while (head != NULL)
