6f8db14b8e86cd2ecae6e00682a3e7d1a0099ae1
FerHdzSchz <fernandohdz915@hotmail.com>
2023-04-13 14:00:00 -0500

    add at the beggining double linked list

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -23,7 +23,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new_node->n = aux_n;
 	new_node->prev = NULL;
-
 	if (*head == NULL)
 	{
 		new_node->next = NULL;
@@ -31,6 +30,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	else
 	{
 		new_node->next = *head;
+		(*head)->prev = new_node;
 	}
 
 	*head = new_node;
