a28b700759eefd1d6459846ebc08a451dd6583a0
FerHdzSchz <fernandohdz915@hotmail.com>
2023-04-14 16:24:28 -0500

    clear bit

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * set_bit - Sets the value of a bit at a given index to 1.
+ * clear_bit - Clears the value of a bit at a given index to 1.
  * @n: A pointer to the bit.
  * @index: The index to set the value at - indices start at 0.
  *
