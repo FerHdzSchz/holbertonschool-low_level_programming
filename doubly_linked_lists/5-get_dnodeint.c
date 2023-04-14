6813354205ab6c5e053624317c0cca73fe7de916
FerHdzSchz <fernandohdz915@hotmail.com>
2023-04-13 22:32:42 -0500

    getting index of node

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -21,15 +21,16 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		return (NULL);
 	}
 	n = 0;
+
 	while (h != NULL)
 	{
-		if (index == (n - 1))
+		n++;
+		if (index == (n-1))
 		{
 			idx_node = h;
 			break;
 		}
 		h = h->next;
-		n++;
 	}
 
 	if (index < (n - 1))
