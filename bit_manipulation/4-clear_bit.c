bd7c080edba1909abf125b32bd646a624ccef573
FerHdzSchz <fernandohdz915@hotmail.com>
2023-04-14 16:23:44 -0500

    clear bit

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -7,7 +7,7 @@
  * Return: If an error occurs - -1.
  *         Otherwise - 1.
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
