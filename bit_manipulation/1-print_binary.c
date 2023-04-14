cd4b36363a17e8e7b48902eabfde9a199b645e12
FerHdzSchz <fernandohdz915@hotmail.com>
2023-04-14 16:02:55 -0500

    print binary representation

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary - prints Dec2bin
+ * @n: unsigned integer
+ *
+*/
+
+void print_binary(unsigned long int n)
+{
+	int c, j;
+	unsigned long int aux_n = n;
+
+	j = 0;
+	for (c = 1; c <= 32; c++)
+		{
+		aux_n >>= 1;
+		if (aux_n > 0)
+		{
+			j++;
+		}
+	}
+
+	for (c = 1; c <= j + 1; c++)
+	{
+		_putchar(n & 1 << j ? '1' : '0');
+		n <<= 1;
+	}
+
+}
