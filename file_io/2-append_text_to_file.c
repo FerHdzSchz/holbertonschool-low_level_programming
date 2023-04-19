20e39e9f5594f9d4870a6f2a26f6e0a06429d4c2
FerHdzSchz <fernandohdz915@hotmail.com>
2023-04-18 20:16:18 -0500

    file append NULL FILENAME

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -19,6 +19,11 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
 	text_len = 0;
 
 	if (text_content == NULL)
