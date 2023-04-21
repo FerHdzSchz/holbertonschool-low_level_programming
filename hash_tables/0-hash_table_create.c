7471864b42735ab168885a1c17cb4e324851b7b0
FerHdzSchz <fernandohdz915@hotmail.com>
2023-04-20 22:01:47 -0500

    Hash table create

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -20,7 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	table->size = size;
-	table->array = malloc(sizeof(hash_node_t) * size);
+	table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (table->array == NULL)
 	{
